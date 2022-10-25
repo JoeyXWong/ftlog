3e3d90be983533edddead012d8189b26075a78ce
jdubs <jjay9225@gmail.com>
2022-10-24 21:37:58 -0400

    very valid values start

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -1,8 +1,13 @@
 package mytypes
 
+import "strings"
+
 type (
-	MyInt    int
-	MyString string
+	MyInt     int
+	MyString  string
+	MyBuilder struct {
+		Contents strings.Builder
+	}
 )
 
 func (i MyInt) Twice() MyInt {
@@ -11,4 +16,20 @@ func (i MyInt) Twice() MyInt {
 
 func (s MyString) StringLen() int {
 	return len(s)
+}
+
+func (b MyBuilder) Len() int {
+	return 0
+}
+
+func (b MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+
+func (b MyBuilder) ToUpper() string {
+	return strings.ToUpper(b.Contents.String())
+}
+
+func (i *MyInt) Double() {
+	*i *= 2
 }
--- mytypes/mytypes_test.go
+++ mytypes/mytypes_test.go
@@ -30,3 +30,51 @@ func TestMyStringLen(t *testing.T) {
 		t.Errorf("input:%s, want:%d, got:%d", input, want, got)
 	}
 }
+
+func TestMyBuilderHello(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	want := "Hello, Gophers!"
+	got := mb.Hello()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestMyBuilder(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	mb.Contents.WriteString("Hello, ")
+	mb.Contents.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := mb.Contents.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+	wantLen := 15
+	gotLen := mb.Contents.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q: want len %d, got %d", mb.Contents.String(), wantLen, gotLen)
+	}
+}
+
+func TestStringUppercaser(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	mb.Contents.WriteString("hello")
+	want := "HELLO"
+	got := mb.ToUpper()
+	if want != got {
+		t.Errorf("want: %q, got: %q", want, got)
+	}
+}
+
+func TestDouble(t *testing.T) {
+	t.Parallel()
+	var x mytypes.MyInt = 12
+	want := mytypes.MyInt(24)
+	x.Double()
+	if want != x {
+		t.Errorf("want: %d, got: %d", want, x)
+	}
+}
